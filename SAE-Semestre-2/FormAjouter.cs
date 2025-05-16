26f130ab37d61d6642b152958ba2a17f17a9e0ee
DevMithril <tbruyelles@gmail.com>
2025-05-16 15:32:32 +0200

    Initialisation du projet VS WinForms

---
+++ SAE-Semestre-2/FormAjouter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAE_Semestre_2
+{
+    public partial class FormAjouter : Form
+    {
+        public FormAjouter()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SAE-Semestre-2/FormItineraire.cs
@@ -0,0 +1,10 @@
+namespace SAE_Semestre_2
+{
+    public partial class FormItineraire : Form
+    {
+        public FormItineraire()
+        {
+            InitializeComponent();
+        }
+    }
+}
