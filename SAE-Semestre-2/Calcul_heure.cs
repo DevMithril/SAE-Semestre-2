13b969545d66642161913e21ba6fc097dfd5c25f
Alix Gianessi <alix.zarude@gmail.com>
2025-06-04 19:45:40 +0200

    Ajout de la classe CalculTemps

---
+++ SAE-Semestre-2/Calcul_heure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_Semestre_2
+{
+    public class CalculTemps
+    {
+        private string heureDebut;
+        private string heureFin;
+
+        public CalculTemps(string heureDebut, string heureFin)
+        {
+            this.heureDebut = heureDebut;
+            this.heureFin = heureFin;
+        }
+
+        private int ConvertirEnMinutes(string heure)
+        {
+            string[] parties = heure.ToLower().Split('h');
+            int heures = int.Parse(parties[0]);
+            int minutes = int.Parse(parties[1]);
+            return heures * 60 + minutes;
+        }
+
+        public int CalculerDifference()
+        {
+            int debutMinutes = ConvertirEnMinutes(heureDebut);
+            int finMinutes = ConvertirEnMinutes(heureFin);
+            if (finMinutes < debutMinutes)
+            {
+                finMinutes += 24 * 60;
+            }
+
+            return finMinutes - debutMinutes;
+        }
+    }
+
+}
